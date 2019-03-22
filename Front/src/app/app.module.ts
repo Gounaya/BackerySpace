414f4cd502e48cb1622dd41458513fd1f3ac34e1
Miguel Yann <tsambamiguelito@gmail.com>
2019-03-22 14:00:59 +0100

    edit package json

--- Front/src/app/app.module.ts
+++ Front/src/app/app.module.ts
@@ -3,10 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { ProduitsComponent } from './produits/produits.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    ProduitsComponent
+
   ],
   imports: [
     BrowserModule,
