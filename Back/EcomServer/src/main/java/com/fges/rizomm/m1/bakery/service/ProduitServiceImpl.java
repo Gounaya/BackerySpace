379b98ef8d3e0e0e83f993d6a6f8386635674f0c
Redha <benzinered@yahoo.fr>
2019-03-29 17:53:28 +0100

    Adding ProduitService and Cart Service

---
+++ Back/EcomServer/src/main/java/com/fges/rizomm/m1/bakery/service/ProduitServiceImpl.java
@@ -0,0 +1,57 @@
+package com.fges.rizomm.m1.bakery.service;
+
+import com.fges.rizomm.m1.bakery.dao.ProduitRepository;
+import com.fges.rizomm.m1.bakery.entites.Produit;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProduitServiceImpl implements ProduitService {
+
+    private final ProduitRepository produitRepository;
+
+    @Autowired
+    public ProduitServiceImpl(ProduitRepository produitRepository){
+        this.produitRepository = produitRepository;
+    }
+
+   /* @Override
+    public void save(Produit produit) {
+
+    MultipartFile file = produit.getImage();
+
+        if (null != file){
+            try {
+                produit.setPhoto(file.getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        produit.setCreateDate(LocalDateTime.now());
+        produitRepository.save(produit);
+
+    }*/
+
+    @Override
+    public Optional<Produit> find(Long id) {
+        return produitRepository.findById(id);
+    }
+
+    @Override
+    public void delete(Long id) {
+        produitRepository.deleteById(id);
+    }
+
+    @Override
+    public List<Produit> findAll() {
+        return produitRepository.findAll();
+    }
+
+    @Override
+    public Produit update(Produit produit) {
+       return produitRepository.save(produit);
+    }
+}
